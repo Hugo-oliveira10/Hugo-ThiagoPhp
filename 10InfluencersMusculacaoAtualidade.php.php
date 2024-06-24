a1a9cf64ea1caedc03086c408a726c7092cfae02
Hugo-oliveira10 <143663456+Hugo-oliveira10@users.noreply.github.com>
2024-06-23 22:51:07 -0300

    Update 10InfluencersMusculacaoAtualidade.php.php
    Adicionado liks com algumas historias dos Atletas

--- 10InfluencersMusculacaoAtualidade.php.php
+++ 10InfluencersMusculacaoAtualidade.php.php
@@ -1,13 +1,18 @@
 <div class="container">
     <h2>Top 10 Influencers de Musculação da Atualidade</h2>
     <ol>
-        <li>Greg Doucette</li>
-        <li>Jeff Nippard</li>
-        <li>Chris Bumstead</li>
-        <li>Stephanie Buttermore</li>
-        <li>Steve Cook</li>
-        <li>Bret Contreras</li>
-        <li>Bret Contreras</li>
+        <li>Greg Doucette
+        https://www.gregdoucette.com/pages/about</li>
+        <li>Jeff Nippard
+        https://www.researchgate.net/profile/Jeff-Nippard</li>
+        <li>Chris Bumstead
+        https://nutrata.com.br/blog/cbum-altura-idade-peso-e-mr-olympia/</li>
+        <li>Stephanie Buttermore
+        https://www.stephaniebuttermore.com/about/</li>
+        <li>Steve Cook
+        https://www.stevecookhealth.com/my-story</li>
+        <li>Bret Contreras
+        https://bretcontreras.com/about-me/</li>
         <li>Kali Muscle</li>
         <li>Bonny Rebecca</li>
         <li>Jujimufu</li>
